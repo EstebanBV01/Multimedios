3881f81b89e0acb6bf4004b40d22818ed4ed7eb2
EstebanBV01 <estebanbarrantes37@gmail.com>
2021-06-16 23:09:57 -0600

    agregar medidor
    g

--- Multimedios/assets/JS/addMeter.js
+++ Multimedios/assets/JS/addMeter.js
@@ -8,7 +8,7 @@ const validateWaterMeter = async(waterMeterUid) => {
     let docRef= db.collection("WaterMeters").doc(waterMeterUid);
 
     docRef.get().then((doc) => {
-        if (doc.exists) {
+        if (doc.exists&& doc.data().activated==false) {
             
             db.collection("Users").doc(user.uid).update({
                 [waterMeterUid]: {
@@ -31,6 +31,30 @@ const validateWaterMeter = async(waterMeterUid) => {
         }
     }).catch((error) => {
         console.log("Error getting document:", error);
+    });
+
+    } else {
+
+    }
+
+}
+const addWaterMeter = async(waterMeterUid) => {
+    let db = firebase.firestore();
+    let user = firebase.auth().currentUser;
+    
+    if (user) {
+
+    db.collection("WaterMeters").doc(waterMeterUid).set({
+        [waterMeterUid]: {
+            custonName: "Oficina C3",
+            lastValue: 300
+        }
+    })
+    .then(() => {
+        console.log("Document successfully written!");
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
     });
 
     } else {
